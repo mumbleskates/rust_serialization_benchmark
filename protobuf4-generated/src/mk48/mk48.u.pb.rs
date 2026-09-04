25d6c00503932b9206e3027312d3b9b347833ad9
Kent Ross <k@mad.cash>
2026-09-03 17:45:50 -0700

    regenerate

--- protobuf4-generated/src/mk48/mk48.u.pb.rs
+++ protobuf4-generated/src/mk48/mk48.u.pb.rs
@@ -1,4 +1,4 @@
-const _: () = ::protobuf::__internal::assert_compatible_gencode_version("4.35.1-release");
+const _: () = ::protobuf::__internal::assert_compatible_gencode_version("0.36.1-release");
 // This variable must not be referenced except by protobuf generated
 // code.
 pub(crate) static mut prost__mk48__Vector2f_msg_init: ::protobuf::__internal::runtime::MiniTableInitPtr =
--- src/datasets/log/log_buffa/log.__view.rs
+++ src/datasets/log/log_buffa/log.__view.rs
@@ -17,9 +17,11 @@ impl<'a> ::buffa::MessageView<'a> for AddressView<'a> {
     type Owned = super::super::Address;
     fn decode_view(buf: &'a [u8]) -> ::core::result::Result<Self, ::buffa::DecodeError> {
         let __limit = ::core::cell::Cell::new(::buffa::DEFAULT_UNKNOWN_FIELD_LIMIT);
+        let __elem = ::core::cell::Cell::new(::buffa::DEFAULT_ELEMENT_MEMORY_LIMIT);
         <Self as ::buffa::MessageView>::decode_view_ctx(
             buf,
-            ::buffa::DecodeContext::new(::buffa::RECURSION_LIMIT, &__limit),
+            ::buffa::DecodeContext::new(::buffa::RECURSION_LIMIT, &__limit)
+                .with_element_memory(&__elem),
         )
     }
     fn decode_view_with_ctx(
@@ -295,9 +297,11 @@ impl<'a> ::buffa::MessageView<'a> for LogView<'a> {
     type Owned = super::super::Log;
     fn decode_view(buf: &'a [u8]) -> ::core::result::Result<Self, ::buffa::DecodeError> {
         let __limit = ::core::cell::Cell::new(::buffa::DEFAULT_UNKNOWN_FIELD_LIMIT);
+        let __elem = ::core::cell::Cell::new(::buffa::DEFAULT_ELEMENT_MEMORY_LIMIT);
         <Self as ::buffa::MessageView>::decode_view_ctx(
             buf,
-            ::buffa::DecodeContext::new(::buffa::RECURSION_LIMIT, &__limit),
+            ::buffa::DecodeContext::new(::buffa::RECURSION_LIMIT, &__limit)
+                .with_element_memory(&__elem),
         )
     }
     fn decode_view_with_ctx(
@@ -647,9 +651,11 @@ impl<'a> ::buffa::MessageView<'a> for LogsView<'a> {
     type Owned = super::super::Logs;
     fn decode_view(buf: &'a [u8]) -> ::core::result::Result<Self, ::buffa::DecodeError> {
         let __limit = ::core::cell::Cell::new(::buffa::DEFAULT_UNKNOWN_FIELD_LIMIT);
+        let __elem = ::core::cell::Cell::new(::buffa::DEFAULT_ELEMENT_MEMORY_LIMIT);
         <Self as ::buffa::MessageView>::decode_view_ctx(
             buf,
-            ::buffa::DecodeContext::new(::buffa::RECURSION_LIMIT, &__limit),
+            ::buffa::DecodeContext::new(::buffa::RECURSION_LIMIT, &__limit)
+                .with_element_memory(&__elem),
         )
     }
     fn decode_view_with_ctx(
